acd82b3d9b5b774a93e0060351106c5aee4f8a33
l-lin <louis.lin.87@gmail.com>
2014-02-14 00:33:06 +0100

    Change graphical charter! wut!

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 (function() {
 	'use strict';
-	angular.module('llinApp', ['ui.bootstrap']);
-	
+	angular.module('llinApp', ['ui.bootstrap']).
+	service('$llinService', function($log) {
+		this.saveToLocalStorage = function save(key, value) {
+			if(angular.isDefined(localStorage)) {
+				$log.debug('Saving ' + value + ' in localeStorage[\'' + key + '\']');
+				localStorage.setItem(key, value);
+			} else {
+				$log.warn('Could not save ' + value + ' in localeStorage[\'' + key + '\']');
+			}
+		}
+	}).
+	controller('llinCtrl', function($scope, $llinService) {
+		var KEY = 'LLIN_SIDEBAR_COLLAPSED_KEY',
+			collapsed;
+		if(angular.isDefined(localStorage)) {
+			collapsed = localStorage.getItem(KEY);
+			if(collapsed === null || angular.isUndefined(collapsed)) {
+				collapsed = false;
+			} else {
+				collapsed = collapsed === 'true';
+			}
+		}
+		$scope.collapsed = collapsed;
+		$scope.collapseSidebar = function collapseSidebar() {
+			$scope.collapsed = !$scope.collapsed;
+			$llinService.saveToLocalStorage(KEY, $scope.collapsed);
+		};
+	});
 })();
