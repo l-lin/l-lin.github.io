2d1c5e3c7bfec7941fd04982a41e4c6d98b888ca
Louis Lin <lin.louis@pm.me>
2019-06-24 09:33:59 +0200

    post(doc+api): add mind maps to software doc & API management

---
+++ content/mindmaps/architecture.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1561128977743" ID="ID_1986140700" MODIFIED="1561128986088" TEXT="Architecture">
+<node CREATED="1561128987834" ID="ID_757123609" MODIFIED="1561129009483" POSITION="right" TEXT="Minimalist, messy &amp; inconsistent">
+<node CREATED="1561129127535" ID="ID_1520368827" MODIFIED="1561129140392" TEXT="Enable autonomy, adaptability &amp; time to value"/>
+<node CREATED="1561129144959" ID="ID_530859140" MODIFIED="1561129153068" TEXT="!= cost, reuse &amp; consistency"/>
+</node>
+</node>
+</map>
---
+++ content/post/2019/2019-06-24-software_documentation.md
@@ -0,0 +1,18 @@
+---
+title: "Mind map - Software documentation"
+date: 2019-06-24T09:01:16+02:00
+imageUrl: ""
+tags: ["mindmaps", "documentation"]
+categories: ["post"]
+comment: true
+toc: false
+autoCollapseToc: false
+contentCopyright: false
+---
+
+<!--more-->
+
+{{% figure class="center" src="/images/mindmaps/software_documentation.svg" alt="Software documentation" title="Software documentation" %}}
+
+Mind map of the thesis on !["Software Documentation - Building and maintaining Artefacts of communication"](http://www.site.uottawa.ca/~tcl/gradtheses/aforward/aforward_thesis.doc) by Andrew Forward.
+
