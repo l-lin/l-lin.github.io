d68c6569d763fa79e337359a5da6e2eb4957431b
Louis Lin <lin.louis@pm.me>
2020-09-21 09:31:22 +0200

    post(not giving a fck): add mindmap

---
+++ content/mindmaps/not_giving_a_fuck.mm
@@ -0,0 +1,335 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1600672000951" ID="ID_1420978753" MODIFIED="1600673135817" TEXT="Not giving a fuck">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1600672064633" ID="ID_1431997808" MODIFIED="1600673135735" POSITION="right" TEXT="Don&apos;t try">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1600672070160" ID="ID_1333631208" MODIFIED="1600673135738" TEXT="Feeback loop from hell">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672088887" ID="ID_140629137" MODIFIED="1600673135739" TEXT="Desired for more positive exp = negative exp">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672110947" ID="ID_1863948866" MODIFIED="1600673135742" TEXT="Acceptance of negative exp = positive exp">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672136948" ID="ID_1341080776" MODIFIED="1600673135745" TEXT="!= being a psychopath">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672166126" ID="ID_40106564" MODIFIED="1600673135746" TEXT="Being comfortable with being different">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672187381" ID="ID_238795794" MODIFIED="1600673135760" TEXT="Choose what to give a fuck">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672202155" ID="ID_314510060" MODIFIED="1600673135760" TEXT="Choose smt more important than adversity">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1600672219935" ID="ID_1374131692" MODIFIED="1600673135762" POSITION="left" TEXT="Happiness is a problem">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1600672234057" ID="ID_749365570" MODIFIED="1600673135763" TEXT="Happiness is not an equation">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672244233" ID="ID_1329515171" MODIFIED="1600673135764" TEXT="&quot;If I can have this, then I will be happy&quot;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672267060" ID="ID_292477291" MODIFIED="1600673135765" TEXT="Solve problems">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1600672272668" ID="ID_903156709" MODIFIED="1600673135765" TEXT="Choose your struggles">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672284670" ID="ID_1393088279" MODIFIED="1600673135766" TEXT="Don&apos;t choose the destination">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672301202" ID="ID_270111056" MODIFIED="1600673135767" TEXT="Emotion are feedbacks">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672309014" ID="ID_499763671" MODIFIED="1600673135768" TEXT="Question your emotions">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1600672317906" ID="ID_88771824" MODIFIED="1600673135769" POSITION="right" TEXT="You&apos;re not special">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1600672323576" ID="ID_1550482588" MODIFIED="1600673135770" TEXT="Acknowledge problems openly and honestly">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672340886" ID="ID_1539653176" MODIFIED="1600673135771" TEXT="Accept your mundane existence">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672351612" ID="ID_303522064" MODIFIED="1600673135771" TEXT="Appreciate ordinary stuffs">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672363730" ID="ID_424617362" MODIFIED="1600673135772" TEXT="Entitlement">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672367605" ID="ID_698480438" MODIFIED="1600673135773" TEXT="&quot;I rock, so I deserve special treatment&quot;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672382525" ID="ID_1040120092" MODIFIED="1600673135773" TEXT="&quot;I suck, so I deserve special treatment&quot;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672399458" ID="ID_385973912" MODIFIED="1600673135774" TEXT="Technology brings new psychological pbs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672418908" ID="ID_1239965288" MODIFIED="1600673135775" TEXT="Social media cover successes, no failures">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672437752" ID="ID_297425356" MODIFIED="1600673135775" TEXT="Must be exceptional to be happy">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1600672450225" ID="ID_1820961123" MODIFIED="1600673135776" POSITION="left" TEXT="Value of suffering">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1600672460366" ID="ID_347232239" MODIFIED="1600673135777" TEXT="Change the problem">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672469846" ID="ID_1922919060" MODIFIED="1600673135777" TEXT="Change what you value">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672474866" ID="ID_410540591" MODIFIED="1600673135778" TEXT="Change how you measure failure / success">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672491943" ID="ID_1286037449" MODIFIED="1600673135778" TEXT="Bad values">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672495653" ID="ID_1113242258" MODIFIED="1600673135779" TEXT="Pleasure">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672498872" ID="ID_1300884391" MODIFIED="1600673135779" TEXT="Material success">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672502269" ID="ID_1425776231" MODIFIED="1600673135780" TEXT="Always being right">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672507023" ID="ID_1191512561" MODIFIED="1600673135780" TEXT="Staying positive">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672513510" ID="ID_1843927673" MODIFIED="1600673135781" TEXT="Reliant on external events">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672519240" ID="ID_1573077139" MODIFIED="1600673135781" TEXT="Good values">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672526844" ID="ID_779237422" MODIFIED="1600673135782" TEXT="Reality based">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672531451" ID="ID_1836346122" MODIFIED="1600673135782" TEXT="Socially constructive">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672536686" ID="ID_755184711" MODIFIED="1600673135782" TEXT="Immediate">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672539315" ID="ID_855829486" MODIFIED="1600673135783" TEXT="Controllable">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672546821" ID="ID_1696963235" MODIFIED="1600673135783" TEXT="Achieve internally">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672560663" ID="ID_74606032" MODIFIED="1600673135784" TEXT="Values are about priorities">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1600672572531" ID="ID_1434866249" MODIFIED="1600673135785" POSITION="right" TEXT="You are always choosing">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1600672578379" ID="ID_448796949" MODIFIED="1600673135785" TEXT="Being forced">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672589333" ID="ID_1242625884" MODIFIED="1600673135786" TEXT="Feeling victimized">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672594596" ID="ID_1519525295" MODIFIED="1600673135786" TEXT="Feeling miserable">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672604447" ID="ID_1748652865" MODIFIED="1600673135787" TEXT="Choosing empower">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1600672616016" ID="ID_1602567589" MODIFIED="1600673135787" TEXT="Fault">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672618699" ID="ID_819136212" MODIFIED="1600673135788" TEXT="Past tense">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672623497" ID="ID_595771506" MODIFIED="1600673135788" TEXT="Responsability">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672626681" ID="ID_1369143091" MODIFIED="1600673135789" TEXT="Present tense">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672631005" ID="ID_586205059" MODIFIED="1600673135789" TEXT="My situation is my responsibility alone">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1600672650208" ID="ID_69593928" MODIFIED="1600673135790" POSITION="left" TEXT="You&apos;re wrong">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1600672654514" ID="ID_300087572" MODIFIED="1600673135790" TEXT="Assumptions">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672660930" ID="ID_1470230220" MODIFIED="1600673135791" TEXT="Block your path">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672665806" ID="ID_1856727285" MODIFIED="1600673135791" TEXT="Don&apos;t dare because you assume the worst">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672692924" ID="ID_1920739121" MODIFIED="1600673135792" TEXT="Certainty">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672697152" ID="ID_1610579379" MODIFIED="1600673135792" TEXT="Clouds judgment">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672706144" ID="ID_1032042352" MODIFIED="1600673135793" TEXT="Don&apos;t find yourself">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672718843" ID="ID_517719277" MODIFIED="1600673135793" TEXT="Can cement you into a strict role">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672732692" ID="ID_920404319" MODIFIED="1600673135794" TEXT="Saddle with unnecessary expectations">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672747755" ID="ID_200535519" MODIFIED="1600673135794" TEXT="Your values can deceive you">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1600672754143" ID="ID_706305382" MODIFIED="1600673135794" TEXT="Remain humble"/>
+<node COLOR="#111111" CREATED="1600672763988" ID="ID_1003770031" MODIFIED="1600673135794" TEXT="Accept difference in others"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672775580" ID="ID_1649884317" MODIFIED="1600673135795" TEXT="Your problem is not special">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672783893" ID="ID_406610306" MODIFIED="1600673135795" TEXT="Less special =&gt; less threats">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1600672796216" ID="ID_366790119" MODIFIED="1600673135796" POSITION="right" TEXT="Failure is the way forward">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1600672800922" ID="ID_1215842665" MODIFIED="1600673135797" TEXT="Do something principle">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672814389" ID="ID_827380312" MODIFIED="1600673135797" TEXT="Do and the rest will follow">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672820551" ID="ID_1896430629" MODIFIED="1600673135798" TEXT="Change the process">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1600672828519" ID="ID_272845744" MODIFIED="1600673135798" TEXT="Try action =&gt; inspiration =&gt; motivation"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672850997" ID="ID_742908792" MODIFIED="1600673135798" TEXT="Choose adequate values">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672858560" ID="ID_277604550" MODIFIED="1600673135799" TEXT="Broad">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672868644" ID="ID_668182214" MODIFIED="1600673135799" TEXT="Abstract">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672870876" ID="ID_1598450627" MODIFIED="1600673135799" TEXT="Process oriented">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672873987" ID="ID_1880910914" MODIFIED="1600673135800" TEXT="Metric that is never finished">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672885269" ID="ID_408059380" MODIFIED="1600673135800" TEXT="Pain is part of the process">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672892220" ID="ID_1116693787" MODIFIED="1600673135801" TEXT="Denying pain is denying potential">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672905297" ID="ID_1943695910" MODIFIED="1600673135801" TEXT="To master, you must fail a lot">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672913269" ID="ID_218393566" MODIFIED="1600673135802" TEXT="Embrace failure">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672917128" ID="ID_1496738227" MODIFIED="1600673135802" TEXT="Do not avoid them">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1600672925060" ID="ID_650964165" MODIFIED="1600673135803" POSITION="left" TEXT="Importance of saying no">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1600672933432" ID="ID_621131024" MODIFIED="1600673135803" TEXT="Commitment">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672937492" ID="ID_1907174116" MODIFIED="1600673135804" TEXT="Reject alternatives">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600672953824" ID="ID_1604849282" MODIFIED="1600673135804" TEXT="More is not always better">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600672959374" ID="ID_923054848" MODIFIED="1600673135804" TEXT="Boundaries">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600672966984" ID="ID_1531056260" MODIFIED="1600673135805" TEXT="Weak">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1600672972648" ID="ID_1508151121" MODIFIED="1600673135805" TEXT="Avoid responsibilities"/>
+<node COLOR="#111111" CREATED="1600672985002" ID="ID_1802949345" MODIFIED="1600673135805" TEXT="Take responsibility for their partner&apos;s pb"/>
+<node COLOR="#111111" CREATED="1600672998098" ID="ID_1668284836" MODIFIED="1600673135805" TEXT="Beware of traps">
+<node COLOR="#111111" CREATED="1600673004715" ID="ID_345578419" MODIFIED="1600673135806" TEXT="Don&apos;t be the saver of the victim"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1600673018668" ID="ID_1971005859" MODIFIED="1600673135806" TEXT="Strong = take their responsibilities">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1600673031446" ID="ID_222523625" MODIFIED="1600673135806" TEXT="For their own values &amp; pbs"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600673042901" ID="ID_691900652" MODIFIED="1600673135807" TEXT="Relationships">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600673048931" ID="ID_1078608828" MODIFIED="1600673135807" TEXT="Health">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1600673068204" ID="ID_1674010233" MODIFIED="1600673135807" TEXT="Willingness of each to reject &amp; be rejected"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600673081464" ID="ID_455333371" MODIFIED="1600673135808" TEXT="Rejection">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600673084116" ID="ID_767637764" MODIFIED="1600673135808" TEXT="To value X, we must reject non-X">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1600673094667" ID="ID_1460230471" MODIFIED="1600673135809" TEXT="Honesty rather than false politeness">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1600673107066" ID="ID_903907627" MODIFIED="1600673135809" TEXT="Accept bad feedbacks">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1600673112556" ID="ID_1525732526" MODIFIED="1600673135815" TEXT="Pretense / deception">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ content/post/2020/2020-09-21-not_giving_a_fuck.md
@@ -0,0 +1,19 @@
+---
+title: "The subtle art of not giving a fck"
+date: 2020-09-21T09:26:31+02:00
+imageUrl: "https://s3-us-west-2.amazonaws.com/tabs.web.media/c/2/c2sk/c2sk-square-orig.jpg"
+tags: ["mindmaps"]
+categories: ["post"]
+comment: true
+toc: false
+autoCollapseToc: false
+contentCopyright: false
+---
+
+
+<!--more-->
+
+{{< figure class="center" src="/images/mindmaps/not_giving_a_fuck.svg" alt="Not giving a fck" title="Not giving a fck" >}}
+
+Mind map of the book on !["The subtle art of not giving a fck"](https://www.amazon.fr/Subtle-Art-Not-Giving-Counterintuitive/dp/0062641549/ref=sr_1_1?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=35M5AICJQN2TG&dchild=1&keywords=not+giving+a+fck&qid=1600673284&sprefix=not+givin%2Caps%2C183&sr=8-1) by Mark Manson.
+
