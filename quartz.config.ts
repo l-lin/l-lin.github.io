4cad9961facff57d55d4ed992fee84e97ccd92be
Louis Lin <lin.louis@pm.me>
2024-07-24 08:40:05 +0200

    config: disable RSS

--- quartz.config.ts
+++ quartz.config.ts
@@ -77,7 +77,7 @@ const config: QuartzConfig = {
       Plugin.ContentPage(),
       Plugin.FolderPage(),
       Plugin.TagPage(),
-      Plugin.ContentIndex({ enableSiteMap: true }),
+      Plugin.ContentIndex({ enableSiteMap: true, enableRSS: false }),
       Plugin.Assets(),
       Plugin.Static(),
       Plugin.NotFoundPage(),
